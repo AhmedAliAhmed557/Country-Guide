b861cd9e3845b1782727817f405439e326e7f444
Ahmed Ali Ahmed <106572516+AhmedAliAhmed557@users.noreply.github.com>
2023-11-14 14:04:00 +0200

    Country Guide
    Upload fills

---
+++ main.js
@@ -0,0 +1,77 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("search-inp");
+let result = document.getElementById("result");
+
+function capitalizeFirstLetter(inputString) {
+  return inputString.charAt(0).toUpperCase() + inputString.slice(1);
+}
+const capitalizeInput = () => {
+  countryInp.value = capitalizeFirstLetter(countryInp.value);
+};
+
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let modefiedName = countryName.charAt(0).toUpperCase() + countryName.slice(1);
+  let URL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+  console.log(modefiedName);
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      let info = data;
+
+      countryInfoBody(info);
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `
+        <h3>The input field cannot be empty</h3>
+      `;
+      } else {
+        result.innerHTML = `
+        <h3>Please a vaild country name</h3>
+      `;
+      }
+    });
+});
+
+const countryInfoBody = (data) => {
+  result.innerHTML = `
+        <img class="flag-img" src=${data[0].flags.svg} alt="Country Image">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+          <div class="data-wrapper">
+            <h4>Capital: </h4>
+            <span>${data[0].capital[0]}</span>
+          </div>
+        </div>
+        <div class="wrapper">
+          <div class="data-wrapper">
+            <h4>Continent: </h4>
+            <span>${data[0].continents[0]}</span>
+          </div>
+        </div>
+        <div class="wrapper">
+          <div class="data-wrapper">
+            <h4>Common Languages: </h4>
+            <span>${Object.values(data[0].languages)
+              .toString()
+              .split(",")
+              .join(", ")}</span>
+          </div>
+        </div>
+        <div class="wrapper">
+          <div class="data-wrapper">
+            <h4>Population: </h4>
+            <span>${data[0].population}</span>
+          </div>
+        </div>
+        <div class="wrapper">
+          <div class="data-wrapper">
+            <h4>Currencie: </h4>
+            <span>${
+              data[0].currencies[Object.keys(data[0].currencies)].name
+            } - ${Object.keys(data[0].currencies)}</span>
+          </div>
+        </div>
+  `;
+};
